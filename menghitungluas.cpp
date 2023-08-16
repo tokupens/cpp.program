c17848a4693fa993811757aa12bdb0db7a1030c6
Tokupensx07 <119869342+tokupens@users.noreply.github.com>
2023-08-16 14:45:06 +0700

    Create menghitungluas.cpp

---
+++ menghitungluas.cpp
@@ -0,0 +1,55 @@
+#include
+
+Using namespace std;
+
+Int main(){
+
+            int l, s;
+
+            cout << "===============================\n";
+
+            cout << "program menghitung luas persegi\n";
+
+            cout << "===============================\n";
+
+            cout << "masukkan panjang sisi persegi: ";
+
+            cin >> s;
+
+            l = s*s;
+
+            cout << "luas persegi adalah "<< l << endl;
+
+            return 0;
+
+}
+
+Program menghitung luas persegi panjang
+
+#include
+
+#include "conio.h"
+
+Using namespace std;
+
+Int main(){
+
+            int p, l;
+
+            cout<<"========================================"<
+
+            cout<<"program penghitung luas persegi panjang\n"<
+
+            cout<<"========================================"<
+
+            cout<<"masukkan nilai panjang= ";
+
+            cin>>p;
+
+            cout<<"masukkan nilai lebar= ";
+
+            cin>>l;
+
+            cout<<"luas persegi panjang="; cout<<(p*l);
+
+            getch();
